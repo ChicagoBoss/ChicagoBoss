61ee82e45d8d1e86d837183404dfa602ea4351d9
Evan Miller <emmiller@gmail.com>
2010-06-05 14:12:52 -0500

    More test coverage for /admin and /doc

--- Test/admin_test.erl
+++ Test/admin_test.erl
@@ -39,7 +39,8 @@ root_test() ->
                             [
                               "Submit valid greeting",
                               fun(Response4) ->
-                                  boss_test:submit_form("create", [{"greeting_text", "Hello"}], Response4,
+                                  ValidGreetingText = "Hello",
+                                  boss_test:submit_form("create", [{"greeting_text", ValidGreetingText}], Response4,
                                     fun(Res) -> [boss_assert:http_redirect(Res)] end,
                                     [
                                       "Redirect",
@@ -63,12 +64,23 @@ root_test() ->
                                                                   boss_assert:http_redirect(Res),
                                                                   boss_assert:redirect_location(GreetingUrl, Res)
                                                                 ]
-                                                            end, []) end ]) end ]) end ]) end,
+                                                            end, []) end ]) end,
+                                              "Back to greeting list",
+                                              fun(Response6) ->
+                                                  boss_test:follow_link("Back to the greeting list", Response6,
+                                                    fun(Res) ->
+                                                        [
+                                                          boss_assert:http_ok(Res),
+                                                          boss_assert:tag_with_text("td", ValidGreetingText, Res)
+                                                        ]
+                                                    end, []) end ]) end ]) end,
                               "Submit invalid greeting",
                               fun(Response4) ->
                                   boss_test:submit_form("create", [{"greeting_text", "Hi"}], Response4,
-                                    fun(Res) ->
-                                        [boss_assert:http_ok(Res)]
+                                    fun(Res) -> [
+                                        boss_assert:http_ok(Res),
+                                        boss_assert:tag_with_text("li", "Greeting must be at least 4 characters long!", Res)
+                                    ]
                                     end, []) end ]) end ]) end ])
       end,
       "EDoc at /doc",
@@ -77,6 +89,18 @@ root_test() ->
             fun(Res) -> [
                   boss_assert:http_ok(Res),
                   boss_assert:link_with_text("Function Index", Res),
-                  boss_assert:link_with_text("id/0*", Res)
+                  boss_assert:link_with_text("Function Details", Res),
+                  boss_assert:link_with_text("attribute_names/0*", Res),
+                  boss_assert:link_with_text("attributes/0*", Res),
+                  boss_assert:link_with_text("attributes/1*", Res),
+                  boss_assert:link_with_text("belongs_to/0*", Res),
+                  boss_assert:link_with_text("belongs_to_names/0*", Res),
+                  boss_assert:link_with_text("greeting_text/0*", Res),
+                  boss_assert:link_with_text("greeting_text/1*", Res),
+                  boss_assert:link_with_text("id/0*", Res),
+                  boss_assert:link_with_text("id/1*", Res),
+                  boss_assert:link_with_text("save/0*", Res),
+                  boss_assert:link_with_text("validate/0*", Res),
+                  boss_assert:link_with_text("validation_tests/0*", Res)
                 ] 
             end, []) end ]).
