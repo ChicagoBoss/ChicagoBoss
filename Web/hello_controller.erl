82b6af064a9d71e2622246e2372937f2edc9c840
Evan Miller <emmiller@gmail.com>
2010-06-23 16:31:13 -0500

    Prep for 0.3.10

--- Web/hello_controller.erl
+++ Web/hello_controller.erl
@@ -2,7 +2,7 @@
 -compile(export_all).
 
 world('GET', []) ->
-    Greetings = boss_db:find('greeting', [], 1000),
+    Greetings = boss_db:find('greeting', []),
     case length(Greetings) of
         0 -> 
             {ok, SavedGreeting} = (greeting:new(id, "Boss says Hello!")):save(),
