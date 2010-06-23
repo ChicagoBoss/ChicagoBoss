ba6ad72ed75148ef23cbf9d22c0e693f3b7ea266
Evan Miller <emmiller@gmail.com>
2010-06-23 16:07:24 -0500

    DB driver guide.

--- src/boss/boss_db_driver.erl
+++ src/boss/boss_db_driver.erl
@@ -4,8 +4,8 @@
 %% @spec behaviour_info( atom() ) -> [ {Function::atom(), Arity::integer()} ] | undefined
 behaviour_info(callbacks) ->
     [
-        {find, 1}, {find, 6}, {count, 2}, {delete, 1}, {counter, 1}, 
-        {incr, 2}, {save_record, 1}
+        {start, 0}, {stop, 0}, 
+        {find, 1}, {find, 6}, {count, 2}, {delete, 1}, {counter, 1}, {incr, 2}, {save_record, 1}
     ];
 behaviour_info(_Other) ->
     undefined.
