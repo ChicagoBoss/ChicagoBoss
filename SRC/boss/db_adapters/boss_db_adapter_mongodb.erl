db4223ef41b3e32e4873fc00f48bb5e9f69e41e2
Alex Marandon <contact@alexmarandon.com>
2011-02-27 23:21:19 +0700

    Cleanup code

--- SRC/boss/db_adapters/boss_db_adapter_mongodb.erl
+++ SRC/boss/db_adapters/boss_db_adapter_mongodb.erl
@@ -58,14 +58,8 @@ find(Conn, Type, Conditions, Max, Skip, Sort, SortOrder) when is_atom(Type), is_
         true ->
             Collection = type_to_collection(Type),
             Res = execute(Conn, fun() ->
-                    SortOrder1 = case SortOrder of 
-                        str_ascending -> 1;
-                        num_ascending -> 1;
-                        str_descending -> -1;
-                        num_descending -> -1
-                    end,
                     Selector = build_conditions(Conditions, 
-                                                {Sort, SortOrder1}),
+                                                {Sort, pack_sort_order(SortOrder)}),
                     mongo:find(Collection, Selector, [],
                                Skip, Max) 
                 end),
@@ -132,9 +126,6 @@ delete(Conn, Id) when is_list(Id) ->
     end.
 
 
-is_id_attr(AttrName) ->
-    lists:suffix("_id", atom_to_list(AttrName)).
-
 save_record(Conn, Record) when is_tuple(Record) ->
     Type = element(1, Record),
     Collection = type_to_collection(Type),
@@ -142,7 +133,7 @@ save_record(Conn, Record) when is_tuple(Record) ->
         id ->
             PropList = lists:map(fun({K,V}) -> 
                             case is_id_attr(K) of
-                                true -> {K, boss_to_mongo_id(V)};
+                                true -> {K, pack_id(V)};
                                 false -> {K, pack_value(V)}
                             end
                 end, 
@@ -151,10 +142,10 @@ save_record(Conn, Record) when is_tuple(Record) ->
         DefinedId when is_list(DefinedId) ->
             PropList = lists:map(fun({K,V}) ->
                             case K of 
-                                id -> {'_id', boss_to_mongo_id(DefinedId)};
+                                id -> {'_id', pack_id(DefinedId)};
                                 _ -> 
                                     case is_id_attr(K) of
-                                        true -> {K, boss_to_mongo_id(V)};
+                                        true -> {K, pack_id(V)};
                                         false -> {K, pack_value(V)}
                                     end
                             end
@@ -167,7 +158,7 @@ save_record(Conn, Record) when is_tuple(Record) ->
     case Res of
         {ok, ok} -> {ok, Record};
         {ok, Id} -> 
-            {ok, Record:id(mongo_to_boss_id(Type, Id))};
+            {ok, Record:id(unpack_id(Type, Id))};
         {failure, Reason} -> {error, Reason};
         {connection_failure, Reason} -> {error, Reason}
     end.
@@ -178,35 +169,16 @@ push(_Conn, _Depth) -> ok.
 
 pop(_Conn, _Depth) -> ok.
 
-%%
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Internal functions
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %%
-
-boss_to_mongo_op(BossOperator) ->
-    OperatorsMap = [
-        {'not_equals', '$ne'},
-        {'gt', '$gt'},
-        {'ge', '$gte'},
-        {'lt', '$lt'},
-        {'le', '$lte'},
-        {'in', '$in'},
-        {'not_in', '$nin'}
-    ],
-    proplists:get_value(BossOperator, OperatorsMap).
-
-where_clause(Format, Params) ->
-    erlang:iolist_to_binary(
-                io_lib:format(Format, Params)).
-
-multiple_where_clauses_string(Format, Key, ValueList, Operator) ->
-    ClauseList = lists:map(fun(Value) ->
-                lists:flatten(io_lib:format(Format, [Key, Value]))
-        end, ValueList),
-        string:join(ClauseList, " " ++ Operator ++ " ").
-
-multiple_where_clauses(Format, Key, ValueList, Operator) ->
-    erlang:iolist_to_binary(multiple_where_clauses_string(Format, Key,
-            ValueList, Operator)).
+%% Query generation
+%%
 
 build_conditions(Conditions) ->
     proplist_to_tuple(build_conditions1(Conditions, [])).
@@ -252,7 +224,7 @@ build_conditions1([{Key, Operator, Value}|Rest], Acc) ->
         {'equals', Value} when is_list(Value) -> 
             case is_id_attr(Key) of 
                 true -> 
-                    [{Key, boss_to_mongo_id(Value)}];
+                    [{Key, pack_id(Value)}];
                 false -> 
                     [{Key, list_to_binary(Value)}]
             end;
@@ -274,89 +246,32 @@ build_conditions1([{Key, Operator, Value}|Rest], Acc) ->
 %    ?LOG("Condition", Condition),
     build_conditions1(Rest, lists:append(Condition, Acc)).
 
+where_clause(Format, Params) ->
+    erlang:iolist_to_binary(
+                io_lib:format(Format, Params)).
 
-proplist_to_tuple(PropList) ->
-    ListOfLists = lists:reverse([[K,V]||{K,V} <- PropList]),
-    list_to_tuple(lists:foldl(
-            fun([K, V], Acc) ->
-                    [K,V|Acc] 
-            end, [], ListOfLists)).
+multiple_where_clauses_string(Format, Key, ValueList, Operator) ->
+    ClauseList = lists:map(fun(Value) ->
+                lists:flatten(io_lib:format(Format, [Key, Value]))
+        end, ValueList),
+        string:join(ClauseList, " " ++ Operator ++ " ").
 
-type_to_collection(Type) ->
-    list_to_atom(type_to_table_name(Type)).
+multiple_where_clauses(Format, Key, ValueList, Operator) ->
+    erlang:iolist_to_binary(multiple_where_clauses_string(Format, Key,
+            ValueList, Operator)).
 
-tuple_to_proplist(Tuple) ->
-    List = tuple_to_list(Tuple),
-    Ret = lists:reverse(list_to_proplist(List, [])),
-    Ret.
 
-list_to_proplist([], Acc) -> Acc;
-list_to_proplist([K,V|T], Acc) ->
-    list_to_proplist(T, [{K, V}|Acc]).
-
-mongo_to_boss_id(Type, MongoId) ->
-    lists:concat([Type, "-", binary_to_list(dec2hex(element(1, MongoId)))]).
-
-boss_to_mongo_id(BossId) ->
-    try
-        [_, MongoId] = string:tokens(BossId, "-"),
-        {hex2dec(MongoId)}
-    catch
-        Error:Reason -> 
-            error_logger:warning_msg("Error parsing Boss record id: ~p:~p~n", 
-                [Error, Reason]),
-            []
-    end.
-
-id_type_from_foreign_key(ForeignKey) ->
-    Tokens = string:tokens(atom_to_list(ForeignKey), "_"),
-    NameTokens = lists:filter(fun(Token) -> Token =/= "id" end, 
-        Tokens),
-    string:join(NameTokens, "-").
-
-attr_value(id, MongoDoc) ->
-    proplists:get_value('_id', MongoDoc);
-attr_value(AttrName, MongoDoc) ->
-    proplists:get_value(AttrName, MongoDoc).
-
-mongo_to_boss_value(id, Value, RecordType) ->
-    mongo_to_boss_id(RecordType, Value);
-mongo_to_boss_value(AttrName, {_,_,_} = Value, _) ->
-    case lists:suffix("_time", atom_to_list(AttrName)) of
-        true -> calendar:now_to_datetime(Value);
-        false -> Value
-    end;
-mongo_to_boss_value(_AttrName, [H|T], _) when is_integer(H) ->
-    {integers, [H|T]};
-mongo_to_boss_value(AttrName, Value, _RecordType) ->
-    case is_id_attr(AttrName) and (Value =/= "") of 
-        true -> 
-            IdType = id_type_from_foreign_key(AttrName),
-            mongo_to_boss_id(IdType, Value);
-        false -> 
-            Value
-    end.
+%% 
+%% Boss models introspection
+%%
 
 attribute_names(Type) ->
     DummyRecord = apply(Type, new, lists:seq(1, proplists:get_value(new, Type:module_info(exports)))),
     DummyRecord:attribute_names().
 
-mongo_tuple_to_record(Type, Row) ->
-    MongoDoc = tuple_to_proplist(Row),
-    Args = lists:map(fun(AttrName) ->
-                MongoValue = attr_value(AttrName, MongoDoc),
-                mongo_to_boss_value(AttrName, MongoValue, Type)
-        end, attribute_names(Type)),
-    apply(Type, new, Args).
-
 infer_type_from_id(Id) when is_list(Id) ->
     [Type, _BossId] = string:tokens(Id, "-"),
-    {list_to_atom(Type), type_to_collection(Type), boss_to_mongo_id(Id)}.
-
-type_to_table_name(Type) when is_atom(Type) ->
-    type_to_table_name(atom_to_list(Type));
-type_to_table_name(Type) when is_list(Type) ->
-    inflector:pluralize(Type).
+    {list_to_atom(Type), type_to_collection(Type), pack_id(Id)}.
 
 model_is_loaded(Type) ->
     case code:is_loaded(Type) of
@@ -369,11 +284,55 @@ model_is_loaded(Type) ->
         false -> false
     end.
 
-datetime_to_now(DateTime) ->
-    GSeconds = calendar:datetime_to_gregorian_seconds(DateTime),
-    ESeconds = GSeconds - ?GREGORIAN_SECONDS_1970,
-    {ESeconds div 1000000, ESeconds rem 1000000, 0}.
+is_id_attr(AttrName) ->
+    lists:suffix("_id", atom_to_list(AttrName)).
 
+
+%% 
+%% Conversion between Chicago Boss en MongoDB
+%%
+
+% Find MongoDB collection from Boss type
+type_to_collection(Type) ->
+    list_to_atom(type_to_collection_name(Type)).
+
+type_to_collection_name(Type) when is_atom(Type) ->
+    type_to_collection_name(atom_to_list(Type));
+type_to_collection_name(Type) when is_list(Type) ->
+    inflector:pluralize(Type).
+
+% Convert a tuple return by the MongoDB driver to a Boss record
+mongo_tuple_to_record(Type, Row) ->
+    MongoDoc = tuple_to_proplist(Row),
+    Args = lists:map(fun(AttrName) ->
+                MongoValue = attr_value(AttrName, MongoDoc),
+                unpack_value(AttrName, MongoValue, Type)
+        end, attribute_names(Type)),
+    apply(Type, new, Args).
+
+% Boss and MongoDB have a different conventions to id attributes (id vs. '_id').
+attr_value(id, MongoDoc) ->
+    proplists:get_value('_id', MongoDoc);
+attr_value(AttrName, MongoDoc) ->
+    proplists:get_value(AttrName, MongoDoc).
+
+% Id conversions
+pack_id(BossId) ->
+    try
+        [_, MongoId] = string:tokens(BossId, "-"),
+        {hex2dec(MongoId)}
+    catch
+        Error:Reason -> 
+            error_logger:warning_msg("Error parsing Boss record id: ~p:~p~n", 
+                [Error, Reason]),
+            []
+    end.
+
+unpack_id(Type, MongoId) ->
+    lists:concat([Type, "-", binary_to_list(dec2hex(element(1, MongoId)))]).
+
+
+% Value conversions
 pack_value({{_, _, _}, {_, _, _}} = Val) ->
     datetime_to_now(Val);
 pack_value(V) when is_binary(V) -> pack_value(binary_to_list(V));
@@ -381,12 +340,87 @@ pack_value([H|T]) when is_integer(H) -> list_to_binary([H|T]);
 pack_value({integers, List}) -> List;
 pack_value(V) -> V.
 
+unpack_value(id, Value, RecordType) ->
+    unpack_id(RecordType, Value);
+unpack_value(AttrName, {_,_,_} = Value, _) ->
+    case lists:suffix("_time", atom_to_list(AttrName)) of
+        true -> calendar:now_to_datetime(Value);
+        false -> Value
+    end;
+unpack_value(_AttrName, [H|T], _) when is_integer(H) ->
+    {integers, [H|T]};
+unpack_value(AttrName, Value, _RecordType) ->
+    case is_id_attr(AttrName) and (Value =/= "") of 
+        true -> 
+            IdType = id_type_from_foreign_key(AttrName),
+            unpack_id(IdType, Value);
+        false -> 
+            Value
+    end.
+
+id_type_from_foreign_key(ForeignKey) ->
+    Tokens = string:tokens(atom_to_list(ForeignKey), "_"),
+    NameTokens = lists:filter(fun(Token) -> Token =/= "id" end, 
+        Tokens),
+    string:join(NameTokens, "_").
+
+
+% Operators
+boss_to_mongo_op('not_equals') -> '$ne';
+boss_to_mongo_op('gt') -> '$gt';
+boss_to_mongo_op('ge') -> '$gte';
+boss_to_mongo_op('lt') -> '$lt';
+boss_to_mongo_op('le') -> '$lte';
+boss_to_mongo_op('in') -> '$in';
+boss_to_mongo_op('not_in') -> '$nin'.
+
+
+% Sort clauses
+pack_sort_order(str_ascending) -> 1;
+pack_sort_order(num_ascending) -> 1;
+pack_sort_order(str_descending) -> -1;
+pack_sort_order(num_descending) -> -1.
+
+
+
+%% 
+%% Generic data structure conversions
+%%
+
+% The mongodb driver uses "associative tuples" which look like:
+%      {key1, Value1, key2, Value2}
+tuple_to_proplist(Tuple) ->
+    List = tuple_to_list(Tuple),
+    Ret = lists:reverse(list_to_proplist(List, [])),
+    Ret.
+
+proplist_to_tuple(PropList) ->
+    ListOfLists = lists:reverse([[K,V]||{K,V} <- PropList]),
+    list_to_tuple(lists:foldl(
+            fun([K, V], Acc) ->
+                    [K,V|Acc] 
+            end, [], ListOfLists)).
+
+list_to_proplist([], Acc) -> Acc;
+list_to_proplist([K,V|T], Acc) ->
+    list_to_proplist(T, [{K, V}|Acc]).
+
+datetime_to_now(DateTime) ->
+    GSeconds = calendar:datetime_to_gregorian_seconds(DateTime),
+    ESeconds = GSeconds - ?GREGORIAN_SECONDS_1970,
+    {ESeconds div 1000000, ESeconds rem 1000000, 0}.
+
+
+%%
+%% Decimal to hexadecimal conversion
+%%
 %% Functions below copied from emongo <https://github.com/boorad/emongo>
 %% 
 %% Copyright (c) 2009 Jacob Vorreuter <jacob.vorreuter@gmail.com> 
 %% Jacob Perkins <japerk@gmail.com> 
 %% Belyaev Dmitry <rumata-estor@nm.ru> 
 %% François de Metz <fdemetz@af83.com>
+%%
 
 dec2hex(Dec) ->
     dec2hex(<<>>, Dec).
@@ -422,4 +456,3 @@ hex0(13) -> $d;
 hex0(14) -> $e;
 hex0(15) -> $f;
 hex0(I) ->  $0 + I.
-
