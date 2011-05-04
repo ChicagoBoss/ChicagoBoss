24ba95340feb748a65349541f6bb19054c046ce6
Evan Miller <emmiller@gmail.com>
2011-05-04 11:00:34 -0500

    Fix admin tests.

--- skel/admin/test/admin_test.erl
+++ skel/admin/test/admin_test.erl
@@ -12,53 +12,59 @@ start() ->
       fun(Response1) ->
           boss_web_test:follow_link("/admin", Response1,
             [ fun boss_assert:http_ok/1,
-              fun(Res) -> boss_assert:tag_with_text("title", "Chicago Boss Admin Interface", Res) end
+              fun(Res) -> boss_assert:tag_with_text("title", "Chicago Boss Admin - General Info", Res) end
             ],
-            [ "Click greeting",
-              fun(Response2) ->
-                  boss_web_test:follow_link("greeting", Response2,
+            [ "Click Models",
+              fun(Response1a) ->
+                  boss_web_test:follow_link("Models", Response1a,
                     [ fun boss_assert:http_ok/1,
-                      fun (Res) -> boss_assert:link_with_text("Documentation for the greeting model", Res) end
+                      fun (Res) -> boss_assert:link_with_text("greeting", Res) end
                     ],
-                    [ "Click + New greeting",
-                      fun({_, GreetingUrl, _, _} = Response3) ->
-                          boss_web_test:submit_form("new", [], Response3,
+                    [ "Click greeting",
+                      fun(Response2) ->
+                          boss_web_test:follow_link("greeting", Response2,
                             [ fun boss_assert:http_ok/1,
-                              fun(Res) -> boss_assert:link_with_text("Back to the greeting list", Res) end
+                              fun (Res) -> boss_assert:link_with_text("Documentation for the greeting model", Res) end
                             ],
-                            [ "Submit valid greeting",
-                              fun(Response4) ->
-                                  ValidGreetingText = "Hello",
-                                  boss_web_test:submit_form("create", [{"greeting_text", ValidGreetingText}], Response4,
-                                    [ fun boss_assert:http_redirect/1 ],
-                                    [ "Redirect",
-                                      fun(Response5) ->
-                                          boss_web_test:follow_redirect(Response5,
-                                            [ fun boss_assert:http_ok/1,
-                                              fun(Res) -> boss_assert:link_with_text("Back to the greeting list", Res) end
-                                            ],
-                                            [ "Delete record",
-                                              fun(Response6) ->
-                                                  boss_web_test:submit_form("delete", [], Response6,
-                                                    [ fun boss_assert:http_ok/1 ],
-                                                    [ "Really delete",
-                                                      fun(Response7) ->
-                                                          boss_web_test:submit_form("delete", [], Response7,
-                                                            [ fun boss_assert:http_redirect/1,
-                                                              fun(Res) -> boss_assert:location_header(GreetingUrl, Res) end
-                                                            ], []) end ]) end,
-                                              "Back to greeting list",
-                                              fun(Response6) ->
-                                                  boss_web_test:follow_link("Back to the greeting list", Response6,
-                                                    [ fun boss_assert:http_ok/1,
-                                                      fun(Res) -> boss_assert:tag_with_text("td", ValidGreetingText, Res) end
-                                                    ], []) end ]) end ]) end,
-                              "Submit invalid greeting",
-                              fun(Response4) ->
-                                  boss_web_test:submit_form("create", [{"greeting_text", "Hi"}], Response4,
+                            [ "Click + New greeting",
+                              fun({_, GreetingUrl, _, _} = Response3) ->
+                                  boss_web_test:submit_form("new", [], Response3,
                                     [ fun boss_assert:http_ok/1,
-                                      fun(Res) -> boss_assert:tag_with_text("li", "Greeting must be at least 4 characters long!", Res) end
-                                    ], []) end ]) end ]) end ]) end,
+                                      fun(Res) -> boss_assert:link_with_text("Back to the greeting list", Res) end
+                                    ],
+                                    [ "Submit valid greeting",
+                                      fun(Response4) ->
+                                          ValidGreetingText = "Hello",
+                                          boss_web_test:submit_form("create", [{"greeting_text", ValidGreetingText}], Response4,
+                                            [ fun boss_assert:http_redirect/1 ],
+                                            [ "Redirect",
+                                              fun(Response5) ->
+                                                  boss_web_test:follow_redirect(Response5,
+                                                    [ fun boss_assert:http_ok/1,
+                                                      fun(Res) -> boss_assert:link_with_text("Back to the greeting list", Res) end
+                                                    ],
+                                                    [ "Delete record",
+                                                      fun(Response6) ->
+                                                          boss_web_test:submit_form("delete", [], Response6,
+                                                            [ fun boss_assert:http_ok/1 ],
+                                                            [ "Really delete",
+                                                              fun(Response7) ->
+                                                                  boss_web_test:submit_form("delete", [], Response7,
+                                                                    [ fun boss_assert:http_redirect/1,
+                                                                      fun(Res) -> boss_assert:location_header(GreetingUrl, Res) end
+                                                                    ], []) end ]) end,
+                                                      "Back to greeting list",
+                                                      fun(Response6) ->
+                                                          boss_web_test:follow_link("Back to the greeting list", Response6,
+                                                            [ fun boss_assert:http_ok/1,
+                                                              fun(Res) -> boss_assert:tag_with_text("td", ValidGreetingText, Res) end
+                                                            ], []) end ]) end ]) end,
+                                      "Submit invalid greeting",
+                                      fun(Response4) ->
+                                          boss_web_test:submit_form("create", [{"greeting_text", "Hi"}], Response4,
+                                            [ fun boss_assert:http_ok/1,
+                                              fun(Res) -> boss_assert:tag_with_text("li", "Greeting must be at least 4 characters long!", Res) end
+                                            ], []) end ]) end ]) end ]) end ]) end,
       "EDoc at /doc",
       fun(Response1) ->
           boss_web_test:follow_link("/doc", Response1,
