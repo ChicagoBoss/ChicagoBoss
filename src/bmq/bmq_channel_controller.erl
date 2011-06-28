ff241dd9e2320fa414e7292b717d13abdc655a8d
Evan Miller <emmiller+github@gmail.com>
2011-06-28 06:43:41 -0500

    Fix lost-message bug in BossMQ

--- src/bmq/bmq_channel_controller.erl
+++ src/bmq/bmq_channel_controller.erl
@@ -6,7 +6,7 @@
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--record(state, {channel, messages = [], subscribers = [], max_age, supervisor}).
+-record(state, {channel, messages = [], subscribers = [], max_age, last_pull, supervisor}).
 
 start_link() ->
     start_link([]).
@@ -18,7 +18,7 @@ init(Options) ->
     MaxAgeSeconds = proplists:get_value(max_age, Options, 60),
     Supervisor = proplists:get_value(supervisor, Options),
     Channel = proplists:get_value(channel, Options),
-    {ok, #state{max_age = MaxAgeSeconds, supervisor = Supervisor, channel = Channel}, 
+    {ok, #state{max_age = MaxAgeSeconds, supervisor = Supervisor, channel = Channel, last_pull = erlang:now()}, 
         MaxAgeSeconds * 1000}.
 
 handle_call(_From, _, State) ->
@@ -28,33 +28,34 @@ handle_cast({From, pull, 'now', Subscriber}, State) ->
     NewSubscribers = [Subscriber|State#state.subscribers],
     gen_server:reply(From, ok),
     {noreply, State#state{subscribers = NewSubscribers}};
-handle_cast({From, pull, Timestamp, Subscriber}, State) ->
-    NewSubscribers = case messages_newer_than_timestamp(Timestamp, State#state.messages) of
-        ReturnMessages when erlang:length(ReturnMessages) > 0 ->
-            Subscriber ! {self(), erlang:now(), ReturnMessages},
-            State#state.subscribers;
-        _ ->
-            [Subscriber|State#state.subscribers]
-    end,
+handle_cast({From, pull, 'last', Subscriber}, State) ->
+    {NewSubscribers, LastPull} = pull_messages(State#state.last_pull, Subscriber, State),
     gen_server:reply(From, ok),
-    {noreply, State#state{subscribers = NewSubscribers}};
+    {noreply, State#state{subscribers = NewSubscribers, last_pull = LastPull}};
+handle_cast({From, pull, Timestamp, Subscriber}, State) ->
+    {NewSubscribers, LastPull} = pull_messages(Timestamp, Subscriber, State),
+    gen_server:reply(From, ok),
+    {noreply, State#state{subscribers = NewSubscribers, last_pull = LastPull}};
 handle_cast({From, poll, undefined}, State) ->
-    gen_server:reply(From, {ok, erlang:now(), State#state.messages}),
-    {noreply, State};
+    Now = erlang:now(),
+    gen_server:reply(From, {ok, Now, lists:map(fun({Msg, _Ts}) -> Msg end, State#state.messages)}),
+    {noreply, State#state{ last_pull = Now }};
 handle_cast({From, poll, Timestamp}, State) ->
     ReturnMessages = messages_newer_than_timestamp(Timestamp, State#state.messages),
-    gen_server:reply(From, {ok, erlang:now(), ReturnMessages}),
-    {noreply, State};
+    Now = erlang:now(),
+    gen_server:reply(From, {ok, Now, lists:map(fun({Msg, _Ts}) -> Msg end, ReturnMessages)}),
+    {noreply, State#state{ last_pull = Now }};
 handle_cast({From, push, Message}, State) ->
     Now = erlang:now(),
-    lists:map(fun(Sub) ->
-                Sub ! {self(), Now, [Message]}
-        end, State#state.subscribers),
+    LastPull = lists:foldr(fun(Sub, _) ->
+                Sub ! {self(), Now, [Message]},
+                Now
+        end, State#state.last_pull, State#state.subscribers),
     gen_server:reply(From, {ok, Now}),
     CurrentMessages = messages_newer_than_timestamp(now_minus_seconds(erlang:now(), 
             State#state.max_age), State#state.messages),
     NewMessages = [{Message, Now}|CurrentMessages],
-    {noreply, State#state{messages = NewMessages, subscribers = []}}.
+    {noreply, State#state{messages = NewMessages, subscribers = [], last_pull = LastPull}}.
 
 terminate(_Reason, _State) ->
     ok.
@@ -81,8 +82,17 @@ messages_newer_than_timestamp(Timestamp, Messages) ->
 
 messages_newer_than_timestamp(_, [], Acc) ->
     lists:reverse(Acc);
-messages_newer_than_timestamp({L1, M1, S1} = Ts, [{Msg, {L2, M2, S2}}|Rest], Acc) when (L2 > L1 orelse (L2 =:= L1 andalso (M2 > M1 orelse (M2 =:= M1 andalso S2 > S1)))) ->
-    messages_newer_than_timestamp(Ts, Rest, [Msg|Acc]);
+messages_newer_than_timestamp({L1, M1, S1} = Ts1, [{Msg, {L2, M2, S2} = Ts2}|Rest], Acc) when (L2 > L1 orelse (L2 =:= L1 andalso (M2 > M1 orelse (M2 =:= M1 andalso S2 > S1)))) ->
+    messages_newer_than_timestamp(Ts1, Rest, [{Msg, Ts2}|Acc]);
 messages_newer_than_timestamp(_, _, Acc) ->
     lists:reverse(Acc).
 
+pull_messages(Timestamp, Subscriber, State) ->
+    Now = erlang:now(),
+    case messages_newer_than_timestamp(Timestamp, State#state.messages) of
+        ReturnMessages when erlang:length(ReturnMessages) > 0 ->
+            Subscriber ! {self(), Now, lists:map(fun({Msg, _Ts}) -> Msg end, ReturnMessages)},
+            {State#state.subscribers, Now};
+        _ ->
+            {[Subscriber|State#state.subscribers], Now}
+    end.
