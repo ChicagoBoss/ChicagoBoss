f8f4fd75a8e6cd064eb65f4c6c798fc31aaf85fb
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2011-09-14 20:25:07 +0200

    absolute paths replaced by relative, changed configuration in template

--- skel/start-dev.sh
+++ skel/start-dev.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 cd `dirname $0`
 exec erl -pa $PWD/ebin \
-     -pa {{src}}/ebin \
-     -pa {{src}}/deps/*/ebin \
+     -pa deps/*/ebin \
      -boss developing_app {{appid}} \
      -boot start_sasl -config boss -s reloader -s boss \
      -sname wildbill
