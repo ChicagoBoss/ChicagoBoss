97679fd97a48332cb539e338748e7672b593625e
Evan Miller <emmiller+github@gmail.com>
2011-11-29 19:21:50 -0600

    Fix Tyrant adapter

--- src/boss/db_adapters/boss_db_adapter_tyrant.erl
+++ src/boss/db_adapters/boss_db_adapter_tyrant.erl
@@ -31,7 +31,7 @@ find(_, Id) when is_list(Id) ->
             {error, Reason}
     end.
 
-find(_, Type, Conditions, Max, Skip, Sort, SortOrder) when is_atom(Type), is_list(Conditions), is_integer(Max),
+find(_, Type, Conditions, Max, Skip, Sort, SortOrder) when is_atom(Type), is_list(Conditions),
                                                         is_integer(Skip), is_atom(Sort), is_atom(SortOrder) ->
     case boss_record_lib:ensure_loaded(Type) of
         true ->
