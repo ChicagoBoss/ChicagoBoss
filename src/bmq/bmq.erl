c15241373b100fc76d257970c1b06959e4199ac3
Tim McNamara <code@timmcnamara.co.nz>
2012-07-28 14:05:21 +1200

    Bugfix: typo
    Added closing bracket. 

--- src/bmq/bmq.erl
+++ src/bmq/bmq.erl
@@ -1,6 +1,6 @@
 -module(bmq).
 
--export([now/1, poll/2, pull/3, push/2].
+-export([now/1, poll/2, pull/3, push/2]).
 
 pull(Channel, Timestamp, Subscriber) ->
     gen_server:call(bmq, {pull, Channel, Timestamp, Subscriber}).
