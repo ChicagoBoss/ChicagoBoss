13761f221352527ac36dbcd6783bd2fe419f6688
chan sisowath <chan.sisowath@gmail.com>
2012-08-07 20:42:18 +0200

    add start public_key and ssl

--- src/boss/boss.erl
+++ src/boss/boss.erl
@@ -19,11 +19,15 @@ ensure_started(App) ->
 %% @doc Start the boss server.
 start() ->
     ensure_started(crypto),
+    ensure_started(public_key),
+    ensure_started(ssl),
     application:start(boss).
 
 %% @spec stop() -> ok
 %% @doc Stop the boss server.
 stop() ->
     Res = application:stop(boss),
+    application:stop(ssl),
+    application:stop(public_key),
     application:stop(crypto),
     Res.
