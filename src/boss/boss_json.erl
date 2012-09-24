035e296ebeffc54a938c630f9c638a8b2820c617
Niclas Axelsson <niclas@burbas.se>
2012-09-24 13:55:55 +0200

    Add formatting support for timestamp-fields in JSON.
    Example: {json, [{my_date, erlang:now(), "H:i"}]}

--- src/boss/boss_json.erl
+++ src/boss/boss_json.erl
@@ -32,15 +32,17 @@ json_data1([{VariableName, [First|_] = Variable}|Rest], ModelList, Acc) when is_
             json_data1(Rest, ModelList, [{VariableName, json_data1(Variable, ModelList, [])}|Acc])
     end;
 json_data1([{VariableName, [First|_] = Variable}|Rest], ModelList, Acc) when is_list(First) ->
-    json_data1(Rest, ModelList, [{VariableName, lists:map(fun(Item) -> 
+    json_data1(Rest, ModelList, [{VariableName, lists:map(fun(Item) ->
                             json_data1(Item, ModelList, [])
                     end, Variable)}|Acc]);
 json_data1([{VariableName, {A, B, C} = Val}|Rest], ModelList, Acc) when is_integer(A), is_integer(B), is_integer(C) ->
-    json_data1(Rest, ModelList, [{VariableName, list_to_binary(erlydtl_filters:date(calendar:now_to_datetime(Val), "F d, Y H:i:s"))}|Acc]);
+    json_data1([{VariableName, Val, "F d, Y H:i:s"}|Rest], ModelList, Acc);
+json_data1([{VariableName, {A, B, C} = Val, Format}|Rest], ModelList, Acc) when is_integer(A), is_integer(B), is_integer(C) ->
+    json_data1(Rest, ModelList, [{VariableName, list_to_binary(erlydtl_filters:date(calendar:now_to_datetime(Val), Format))}|Acc]);
 json_data1([{VariableName, Variable}|Rest], ModelList, Acc) ->
     case boss_model_manager:is_model_instance (Variable, ModelList) of
-        true -> 
+        true ->
             json_data1(Rest, ModelList, [{VariableName, boss_model_manager:to_json(Variable)}|Acc]);
-        false -> 
+        false ->
             json_data1(Rest, ModelList, [{VariableName, Variable}|Acc])
     end.
