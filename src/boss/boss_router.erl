0328fbd9c054e67eeb9f36cb1a71de6f6800f798
Evan Miller <emmiller@gmail.com>
2013-02-08 12:52:30 -0600

    Improve {% url %} tag performance
    1. Replace a table scan of the routes with a dictionary lookup (i.e.
    create a new table for reverse routing)
    
    2. Move the fancy token-matching logic out of the router into the
    calling process.

--- src/boss/boss_router.erl
+++ src/boss/boss_router.erl
@@ -7,7 +7,7 @@
 %% Exported Functions
 %%
 -export([start/0, start/1, stop/0]).
--export([reload/1, route/2, unroute/4, handle/2, get_all/1, set_controllers/2]).
+-export([reload/1, route/2, unroute/5, handle/2, get_all/1, set_controllers/2]).
 
 %%
 %% API Functions
@@ -28,8 +28,32 @@ reload(Pid) ->
 route(Pid, Url) ->
     gen_server:call(Pid, {route, Url}).
 
-unroute(Pid, Controller, Action, Params) ->
-    gen_server:call(Pid, {unroute, Controller, Action, Params}).
+unroute(Pid, Application, Controller, Action, Params) ->
+    case gen_server:call(Pid, {unroute, Controller, Action, Params}) of
+        undefined ->
+            % Do the fancy routing in the calling process
+            % to take some pressure off the router process.
+            % In the future the router needs workers.
+            ControllerModule = list_to_atom(boss_files:web_controller(Application, Controller)),
+            {Tokens, Variables1} = boss_controller_lib:convert_params_to_tokens(Params, ControllerModule, list_to_atom(Action)),
+
+            URL = case Tokens of
+                [] ->
+                    lists:concat(["/", Controller, "/", Action]);
+                _ ->
+                    lists:concat(["/", Controller, "/", Action |
+                            lists:foldr(fun(T, Acc) -> ["/", mochiweb_util:quote_plus(T) | Acc] end, [], Tokens)])
+            end,
+            QueryString = mochiweb_util:urlencode(Variables1),
+            case QueryString of
+                "" ->
+                    URL;
+                _ ->
+                    URL ++ "?" ++ QueryString
+            end;
+        RoutedURL ->
+            RoutedURL
+    end.
 
 handle(Pid, StatusCode) ->
     gen_server:call(Pid, {handle, StatusCode}).
