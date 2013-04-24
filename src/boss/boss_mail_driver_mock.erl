7fbb1a62cffedce2f852ed65c2c70ac9372ba8d6
Evan Miller <emmiller@gmail.com>
2013-04-24 15:16:05 -0500

    Support delivery callbacks when sending email

--- src/boss/boss_mail_driver_mock.erl
+++ src/boss/boss_mail_driver_mock.erl
@@ -1,5 +1,5 @@
 -module(boss_mail_driver_mock).
--export([start/0, stop/1, deliver/4, read/2, push/0, pop/0]).
+-export([start/0, stop/1, deliver/5, read/2, push/0, pop/0]).
 
 start() ->
     register(boss_mock_inbox, spawn(fun() -> loop([]) end)),
@@ -43,7 +43,8 @@ loop([InboxDict|OldState] = State) ->
             loop(OldState)
     end.
 
-deliver(_, FromAddress, ToAddress, BodyFun) ->
+deliver(_, FromAddress, ToAddress, BodyFun, _ResultFun) ->
+    % TODO use ResultFun
     boss_mock_inbox ! {self(), {deliver, FromAddress, ToAddress, BodyFun()}},
     receive
         {boss_mock_inbox, ok} ->
