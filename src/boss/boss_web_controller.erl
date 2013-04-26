774005d4111fd65b466366274ce685544b5c1a8b
Nikolay Garanko <ngaranko@gmail.com>
2013-04-26 15:13:33 +0200

    Merging changes

--- src/boss/boss_web_controller.erl
+++ src/boss/boss_web_controller.erl
@@ -783,14 +783,29 @@ execute_action({Controller, Action, Tokens} = Location, AppInfo, Req, SessionID,
                 AppInfo#boss_app_info.controller_modules, Req, SessionID),
             RequestMethod = Req:request_method(),
 
-            AuthResult = Adapter:before_filter(AdapterInfo, Action, RequestMethod, Tokens),
+            BeforeInfo = before_request(boss_env:get_env(AppInfo#boss_app_info.application, middlewares, []),
+                                        Controller, Action, Req, SessionID, []),
 
-            AuthInfo = case AuthResult of
-                ok ->
-                    {ok, undefined};
-                OtherInfo ->
-                    OtherInfo
-            end,
+            AuthInfo = case BeforeInfo of
+                {ok, BInfo} ->
+                    AuthResult = Adapter:before_filter(AdapterInfo, Action, RequestMethod, Tokens),
+
+                    case AuthResult of
+                        ok ->
+                            {ok, BInfo};
+                        {ok, AuthDetails} ->
+                            case length(BInfo) of
+                                0 ->
+                                    {ok, AuthDetails};
+                                _Other ->
+                                    {ok, lists:merge(BInfo, AuthDetails)}
+                            end;
+                        OtherInfo ->
+                            OtherInfo
+                    end;
+                AnotherInfo ->
+                    AnotherInfo
+                end,
 
             case AuthInfo of
                 {ok, Info} ->
@@ -889,6 +904,18 @@ execute_action({Controller, Action, Tokens} = Location, AppInfo, Req, SessionID,
 handle_news_for_cache(_, _, {Prefix, Key}) ->
     boss_cache:delete(Prefix, Key),
     {ok, cancel_watch}.
+
+before_request([], _Controller, _Action, _Req, _SessionID, Info) ->
+    {ok, Info};
+before_request([Middleware|Middlewares], Controller, Action, Req, SessionID, Info) ->
+    case Middleware:before_(Controller, Action, Req, SessionID) of
+        ok ->
+            before_request(Middlewares, Controller, Action, Req, SessionID, Info);
+        {ok, ExtraInfo} ->
+            before_request(Middlewares, Controller, Action, Req, SessionID, lists:merge(ExtraInfo, Info));
+        Other ->
+            Other
+    end.
 
 process_location(Controller,  [{_, _}|_] = Where, AppInfo) ->
     {_, TheController, TheAction, CleanParams} = process_redirect(Controller, Where, AppInfo),
