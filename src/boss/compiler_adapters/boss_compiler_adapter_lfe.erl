921d83d84403188183176b0d833221070f8192af
Evan Miller <emmiller@gmail.com>
2013-06-29 16:22:03 -0500

    LFE: set proper write directory

--- src/boss/compiler_adapters/boss_compiler_adapter_lfe.erl
+++ src/boss/compiler_adapters/boss_compiler_adapter_lfe.erl
@@ -8,7 +8,22 @@ controller_module(AppName, Controller) -> lists:concat([AppName, "_", Controller
 module_name_for_file(_AppName, File) -> filename:basename(File, ".lfe").
 
 compile_controller(File, Options) ->
-    lfe_comp:file(File, [verbose, return | proplists:get_value(compiler_options, Options, [])]).
+    do_compile(File, Options). 
 
 compile(File, Options) ->
-    lfe_comp:file(File, [verbose, return | proplists:get_value(compiler_options, Options, [])]).
+    do_compile(File, Options).
+
+do_compile(File, Options) ->
+    case lfe_comp:file(File, lfe_compiler_options(Options)) of
+        {ok, Module, _Warnings} ->
+            {ok, Module};
+        Other ->
+            Other
+    end.
+
+lfe_compiler_options(Options) ->
+    CompilerOptions = [verbose, return, proplists:get_value(compiler_options, Options, [])],
+    case proplists:get_value(out_dir, Options) of
+        undefined -> CompilerOptions;
+        OutDir -> [{outdir, OutDir} | CompilerOptions]
+    end.
