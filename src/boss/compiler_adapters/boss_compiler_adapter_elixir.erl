8776edf7e659ff69aec317fe00a340fce44b2ed7
Evan Miller <emmiller@gmail.com>
2013-07-13 08:56:28 -0700

    Elixir fixes

--- src/boss/compiler_adapters/boss_compiler_adapter_elixir.erl
+++ src/boss/compiler_adapters/boss_compiler_adapter_elixir.erl
@@ -1,15 +1,17 @@
 -module(boss_compiler_adapter_elixir).
 -compile(export_all).
 
+-define(SEPARATOR, ".").
+
 file_extensions() -> ["ex"].
 
 controller_module(AppName, Controller) -> 
-    lists:concat(["Elixir", "-", inflector:camelize(atom_to_list(AppName)), "-", 
+    lists:concat(["Elixir", ?SEPARATOR, inflector:camelize(atom_to_list(AppName)), ?SEPARATOR, 
             inflector:camelize(Controller), "Controller"]).
 
 module_name_for_file(AppName, File) -> 
-    "Elixir-"++inflector:camelize(atom_to_list(AppName))++
-    "-"++inflector:camelize(filename:basename(File, ".ex")).
+    "Elixir" ++ ?SEPARATOR ++ inflector:camelize(atom_to_list(AppName)) ++
+    ?SEPARATOR ++ inflector:camelize(filename:basename(File, ".ex")).
 
 compile_controller(File, Options) ->
     boss_elixir_compiler:compile(File, Options).
