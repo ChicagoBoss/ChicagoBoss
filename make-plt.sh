9a117647f1f0524163a1225056f5e12e103bbad5
Zachary Kessin <zkessin@gmail.com>
2013-12-09 13:17:20 +0200

    scripts for dialyzer

--- make-plt.sh
+++ make-plt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 
-PLT=plt/api.plt
+PLT=plt/cb.plt
 echo $PLT
 
 if [ ! -f $PLT ]; then
@@ -10,18 +10,9 @@ if [ ! -f $PLT ]; then
        syntax_tools edoc xmerl \
        --statistics\
        --output_plt $PLT
-   rm deps/riak_core/ebin/riak_core_pb.beam
+   rm deps/riak_core/ebin/*.beam
    echo "********************************************************************************"
-   dialyzer --add_to_plt deps/e2/ebin						--plt $PLT
-   dialyzer --add_to_plt deps/chi2/ebin						--plt $PLT
-   dialyzer --add_to_plt deps/edate/ebin deps/emysql/ebin			--plt $PLT
-   dialyzer --add_to_plt deps/erlcloud/ebin deps/erlydtl/ebin deps/jsx/ebin	--plt $PLT
-   dialyzer --add_to_plt deps/lager/ebin deps/oauth/ebin deps/restc/ebin	--plt $PLT
-   dialyzer --add_to_plt deps/time_interval/ebin deps/vclock/ebin		--plt $PLT
-   dialyzer --add_to_plt deps/webmachine/ebin					--plt $PLT
-   dialyzer --add_to_plt deps/jsxd/ebin deps/uuid/ebin deps/erlando/ebin	--plt $PLT
-   dialyzer --add_to_plt deps/mochiweb/ebin deps/mochiweb_util/ebin		--plt $PLT
-   dialyzer --add_to_plt deps/riak_core/ebin					--plt $PLT
+   dialyzer --add_to_plt deps/*/ebin						--plt $PLT
    echo "********************************************************************************"
    echo ""
 fi
