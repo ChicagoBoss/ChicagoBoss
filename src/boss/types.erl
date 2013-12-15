23c1cda58ddada662ec7f04460c73508e2501d4f
Zachary Kessin <zkessin@gmail.com>
2013-12-15 15:46:57 +0200

    fix an export

--- src/boss/types.erl
+++ src/boss/types.erl
@@ -6,6 +6,6 @@
 -type webserver()               :: 'cowboy' | 'mochiweb_http'.
 -type cb_node()                 :: node().
 -type controller()              :: any().
--type module()                  :: atom().
+
 -export_type([execution_mode/0, application/0, language/0, webserver/0, cb_node/0]).
--export_type([controller/0, module/0]).
+-export_type([controller/0]).
