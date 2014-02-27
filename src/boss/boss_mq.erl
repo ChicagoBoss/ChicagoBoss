48c8fc07ff0ccc4222c90d4a7669379ee88e5db6
me <dmitry.polyanovsky@gmail.com>
2014-02-27 23:42:21 +0200

    fix Edoc generation for API

--- src/boss/boss_mq.erl
+++ src/boss/boss_mq.erl
@@ -20,32 +20,21 @@
 -type mq_return() :: {ok, integer(), [_]}|{error,string()}.
 
 -spec start() -> any().
--spec start(_) -> any().
--spec get_mq_adapater() -> atom().
--spec stop() -> 'ok'.
--spec pull(channel()) -> mq_return().
--spec pull(channel(),undefined|non_neg_integer()) -> mq_return().
--spec pull(channel(),undefined|integer()|{integer(), integer(), integer()},infinity|non_neg_integer()) -> mq_return().
--spec pull_recieve('infinity' | non_neg_integer(),{ok, _}|any()) -> mq_return().
--spec convert_to_ms('infinity' | non_neg_integer()) -> 'infinity' | non_neg_integer().
--spec poll(channel()) -> mq_return().
--spec poll(channel(),last|integer()) -> mq_return().
--spec push(channel(),_) -> {ok, non_neg_integer()}.
--spec now(channel()) ->    non_neg_integer().
--spec make_queue_options() -> [atom()].
-
 start() ->
     MQOptions	= make_queue_options(),
     MQAdapter	= get_mq_adapater(),
     MQOptions1	= [{adapter, list_to_atom("boss_mq_adapter_"++atom_to_list(MQAdapter))}|MQOptions],
     start(MQOptions1).
 
+-spec start(_) -> any().
 start(Options) ->
     boss_mq_sup:start_link(Options).
 
+-spec stop() -> 'ok'.
 stop() ->
     ok.
 
+-spec get_mq_adapater() -> atom().
 get_mq_adapater() ->
     case application:get_env(mq_adapter) of
 	{ok, Val} -> Val;
@@ -64,19 +53,19 @@ make_queue_options() ->
 
 
 
-%% @spec pull( Channel::string() ) -> {ok, Timestamp, [Message]} | {error, Reason}
+-spec pull(channel()) -> mq_return().
 %% @doc Pull messages from the specified `Channel'. If none are in the queue, blocks
 %% until a message is pushed to the queue.
 pull(Channel) ->
     pull(Channel, undefined).
 
-%% @spec pull( Channel::string(), Since::integer() | last | now ) -> {ok, Timestamp, [Message]} | {error, Reason}
+-spec pull(channel(),undefined|non_neg_integer()) -> mq_return().
 %% @doc Pull messages from the specified `Channel' after `Since' (a timestamp returned from a previous `pull'). 
 %% If no such messages are in the queue, blocks until a message is pushed to the queue.
 pull(Channel, Timestamp) ->
     pull(Channel, Timestamp, infinity).
 
-%% @spec pull( Channel::string(), Since::integer() | last | now, Timeout::integer() ) -> {ok, Timestamp, [Message]} | {error, Reason}
+-spec pull(channel(),undefined|integer()|{integer(), integer(), integer()},infinity|non_neg_integer()) -> mq_return().
 %% @doc Pull messages from the specified `Channel' after `Since' (a timestamp returned from a previous `pull'). If no such messages
 %% are in the queue, blocks until a message is pushed to the queue, or until `Timeout' seconds have elapsed.
 pull(Channel, {MegaSecs, Secs, MicroSecs}, Timeout) ->
@@ -87,6 +76,7 @@ pull(Channel, Timestamp, Timeout) when is_list(Channel) ->
     pull_recieve(TimeoutMs, CallStatus).
 
 
+-spec pull_recieve('infinity' | non_neg_integer(),{ok, _}|any()) -> mq_return().
 pull_recieve(TimeoutMs, {ok, PullTime}) ->
     receive
 	{_From, NewTimestamp, Messages} ->
@@ -98,28 +88,29 @@ pull_recieve(TimeoutMs, {ok, PullTime}) ->
 pull_recieve(_, Error) -> Error.
 
 
+-spec convert_to_ms('infinity' | non_neg_integer()) -> 'infinity' | non_neg_integer().
 convert_to_ms(infinity) -> infinity;
 convert_to_ms(Timeout) ->
     Timeout * 1000.
 
 
-%% @spec poll( Channel::string() ) -> {ok, Timestamp, [Message]} | {error, Reason}
+-spec poll(channel()) -> mq_return().
 %% @doc Like `pull/1', but returns immediately if no messages are in the queue.
 poll(Channel) ->
     poll(Channel, last).
 
-%% @spec poll( Channel::string(), Since::integer() | last ) -> {ok, Timestamp, [Message]} | {error, Reason}
+-spec poll(channel(),last|integer()) -> mq_return().
 %% @doc Like `pull/2', but returns immediately if no matching messages are in the queue.
 poll(Channel, Timestamp) when is_list(Channel) ->
     gen_server:call({global, ?MODULE}, {poll, Channel, Timestamp}).
     
 
-%% @spec push( Channel::string(), Message ) -> {ok, Timestamp}
+-spec push(channel(),_) -> {ok, non_neg_integer()}.
 %% @doc Pushes a message to the specified `Channel'.
 push(Channel, Message) when is_list(Channel) ->
     gen_server:call({global, ?MODULE}, {push, Channel, Message}).
 
-%% @spec now( Channel::string() ) -> Timestamp
+-spec now(channel()) ->    non_neg_integer().
 %% @doc Retrieves the current time for the server managing `Channel'.
 now(Channel) when is_list(Channel) ->
     gen_server:call({global, ?MODULE}, {now, Channel}).
