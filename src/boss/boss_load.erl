af94abe829e96a12bb1b59a9e06e63825579ae67
chan <regio_marineris@hotmail.com>
2014-12-18 17:13:14 +0800

    add boss_load server, compile controller, compile model for now

--- src/boss/boss_load.erl
+++ src/boss/boss_load.erl
@@ -1,4 +1,9 @@
 -module(boss_load).
+-behaviour(gen_server).
+-define(SERVER, ?MODULE).
+-export([start_link/0]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
 -export([
         incoming_mail_controller_module/1,
         load_all_modules/2,
@@ -15,6 +20,9 @@
         reload_all/0
     ]).
 -include("boss_web.hrl").
+
+-record(load_state, {last, root}).
+
 
 -ifdef(TEST).
 -compile(export_all).
@@ -49,6 +57,171 @@
 		       {'module', atom() | tuple()}].
 
 -define(CUSTOM_TAGS_DIR_MODULE, '_view_lib_tags').
+
+%% code from project github.com/synrc/active 
+%%
+start_link() -> 
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+init(Opts) -> 
+    error_logger:info_msg("Starting boss_load..."),
+    fs:subscribe(), 
+    erlang:process_flag(priority, low), 
+    {ok, #load_state{last=fresh, root=fs:path()}}.
+
+
+handle_call(_Request, _From, State) -> {reply, ok, State}.
+handle_cast(_Msg, State) -> {noreply, State}.
+handle_info({_Pid, {fs,file_event}, {Path, Flags}}, #load_state{root=Root} = State) ->
+    Cur = path_shorten(filename:split(Root)),
+    P = filename:split(Path),
+
+    Result = case lists:prefix(Cur, P) of
+        true ->
+            Components = P -- Cur,
+            %%error_logger:info_msg("event: ~p ~p", [Components, Flags]),
+            path_event(Components, Flags, State);
+        false ->
+            ok
+    end,
+
+    {noreply, State#load_state{last={event, Path, Flags, Result}}};
+handle_info({load_ebin, Atom}, State) -> 
+    error_logger:info_msg("load module ~p",[Atom]),
+    do_load_ebin(Atom), 
+    {noreply, State#load_state{last={do_load_ebin, Atom}}};
+handle_info(Info, State) -> {noreply, State#load_state{last={unk, Info}}}.
+terminate(_Reason, _State) -> ok.
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
+
+
+path_event(C, [E|_Events], _State) when E =:= created; E =:= modified; E =:= renamed ->
+    case path_filter(C) of 
+        true  -> otp(C); 
+        false -> ignore 
+    end;
+path_event(C, [_E|Events], State) -> 
+    path_event(C, Events, State);
+path_event(_, [], _State) -> done.
+
+otp(["deps",App|Rest]) -> app(deps, App,Rest);
+otp(["apps",App|Rest]) -> app(apps, App,Rest);
+otp([Some|Path])       -> app(top, top(),[Some|Path]);
+otp(_)                 -> ok.
+
+app(deps, App, ["ebin",Module|_]) -> skip; %%load_ebin(App, Module); %%
+app(_, App, ["ebin",Module|_])    -> load_ebin(App, Module);
+app(_, App, ["priv","fdlink"++_]) -> skip;
+app(_, App, ["priv","mac"++_])    -> skip;
+app(_, App, ["priv","windows"++_])-> skip;
+app(_, App, ["priv","linux"++_])  -> skip;
+app(Type, App, Path=["priv"|_]) -> 
+    case hd(lists:reverse(Path)) of
+        ".#" ++ _ -> skip; % mc temp files
+        Else      -> compile_app(Type, App, Path) 
+    end;
+app(Type, App,Path=["include"|_])   -> compile_app(Type, App, Path);
+app(Type, App,Path=["src"|_])       -> compile_app(Type, App, Path);
+app(_, _, _)                        -> ok.
+
+top() -> 
+    lists:last(filename:split(filename:absname(""))).
+
+compile_app(deps, App, Path) -> skip;
+compile_app(top, App, Path = ["src", "controller", _]) ->
+    Filename = filename:join(Path),
+    OutDir = "ebin",
+    error_logger:info_msg("compile controller module ~p -> ~p",[Filename, OutDir]),
+    CompileResult = maybe_compile(Filename, list_to_atom(App), OutDir, fun compile_controller/2),
+    error_logger:info_msg("Compile Result ~p", [CompileResult]);
+
+compile_app(top, App, Path = ["src", "model", _]) ->
+    Filename = filename:join(Path),
+    OutDir = "ebin",
+    error_logger:info_msg("compile model module ~p -> ~p",[Filename, OutDir]),
+    CompileResult = maybe_compile(Filename, list_to_atom(App), OutDir, fun compile_model/2),
+    error_logger:info_msg("Compile Result ~p", [CompileResult]).
+
+
+%% compile_app(top, App, Path = ["src", "view", _]) ->
+%%     Filename = filename:join(Path),
+%%     error_logger:info_msg("compile view module ~p",[Filename]),
+%%     CompileResult = maybe_compile(Filename, list_to_atom(App), undefined, fun compile_view/2),
+%%     error_logger:info_msg("Compile Result ~p", [CompileResult]).
+
+%% compile_app(top, App, Path = ["src", "lib", _]) ->
+%%     Filename = filename:join(Path),
+%%     error_logger:info_msg("compile lib module ~p",[Filename]),
+%%     CompileResult = maybe_compile(Filename, list_to_atom(App), undefined, fun compile_lib/2),
+%%     error_logger:info_msg("Compile Result ~p", [CompileResult]).
+
+load_ebin(App,EName) ->
+    error_logger:info_msg("load ebin: ~p,~p", [App,EName]),    
+    Tokens = string:tokens(EName, "."),
+    case Tokens of
+        [Name, "beam"] -> do_load_ebin(list_to_atom(Name));
+        [Name, "bea#"] ->
+            case monitor_handles_renames() of
+                false ->
+                    erlang:send_after(500, ?SERVER, {load_ebin, list_to_atom(Name)}),
+                    delayed;
+                true ->
+                    ignored
+            end;
+        %%[Name, Smth] -> ok;
+        _ ->
+            error_logger:warning_msg("boss_load: unknown BEAM file: ~p", [EName]),
+            ok
+    end.
+
+do_load_ebin(Module) ->
+    {Module, Binary, Filename} = code:get_object_code(Module),
+    code:load_binary(Module, Filename, Binary),
+    lager:notice("Reloading module: ~p~n\n\r", [Module]),
+    reloaded.
+
+monitor_handles_renames([renamed|_]) -> true;
+monitor_handles_renames([_|Events]) -> monitor_handles_renames(Events);
+monitor_handles_renames([]) -> false.
+monitor_handles_renames() ->
+    case get(monitor_handles_renames) of
+        undefined ->
+            R = monitor_handles_renames(fs:known_events()),
+            put(monitor_handles_renames, R),
+            R;
+        V -> V
+    end.
+
+
+
+%% ["a", "b", ".."] -> ["a"]
+path_shorten(Coms) -> path_shorten_r(lists:reverse(Coms), [], 0).
+
+path_shorten_r([".."|Rest], Acc, Count) -> path_shorten_r(Rest, Acc, Count + 1);
+path_shorten_r(["."|Rest], Acc, Count) -> path_shorten_r(Rest, Acc, Count);
+path_shorten_r([_C|Rest], Acc, Count) when Count > 0 -> path_shorten_r(Rest, Acc, Count - 1);
+path_shorten_r([C|Rest], Acc, 0) -> path_shorten_r(Rest, [C|Acc], 0);
+path_shorten_r([], Acc, _) -> Acc.
+
+%
+% Filters
+%
+
+path_filter(L) -> not lists:any(fun(E) -> not path_filter_dir(E) end, L) andalso path_filter_last(lists:last(L)).
+
+path_filter_dir(".git") -> false;
+path_filter_dir(".hg")  -> false;
+path_filter_dir(".svn") -> false;
+path_filter_dir("CVS")  -> false;
+path_filter_dir("log")  -> false;
+path_filter_dir(_)      -> true.
+
+path_filter_last(".rebarinfo")     -> false;   % new rebars
+path_filter_last("LICENSE")        -> false;
+path_filter_last("4913 (deleted)") -> false;   % vim magical file
+path_filter_last("4913")           -> false;
+path_filter_last(_)                -> true.
+
 
 load_all_modules(Application, TranslatorSupPid) ->
     load_all_modules(Application, TranslatorSupPid, undefined).
