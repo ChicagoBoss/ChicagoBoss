52af9b85fc78163584a88fda2cf7bb660504fac5
chan <regio_marineris@hotmail.com>
2014-12-19 12:26:50 +0800

    compile cb apps from folder apps & deps

--- src/boss/boss_load_lib.erl
+++ src/boss/boss_load_lib.erl
@@ -16,15 +16,33 @@ compile(Type, App, Path, CBApps) ->
 compile_app(_Type, _App, _Path) -> skip.
     
 %% Compile Boss APP
+%% TOP FOLLDER
 compile_cbapp(top, App, Path = ["src", "controller", _]) ->
     Filename = filename:join(Path),
     OutDir = "ebin",
     CompileResult = boss_load:maybe_compile(Filename, list_to_atom(App), OutDir, fun boss_load:compile_controller/2),
-    lager:notice("[controller] Compile Result ~p", [CompileResult]);
+    lager:notice("top [controller] Compile Result ~p", [CompileResult]);
 
 compile_cbapp(top, App, Path = ["src", "model", _]) ->
     Filename = filename:join(Path),
     OutDir = "ebin",
     CompileResult = boss_load:maybe_compile(Filename, list_to_atom(App), OutDir, fun boss_load:compile_model/2),
-    lager:notice("[model] Compile Result ~p", [CompileResult]).
+    lager:notice("top [model] Compile Result ~p", [CompileResult]);
+
+%% IN APPS/DEPS FOLDER, CONTROLLER & MODEL
+compile_cbapp(Type, App, Path = ["src", "controller", _]) ->
+    Folder = atom_to_list(Type),
+    Filename = filename:join([Folder, App] ++ Path),
+    OutDir = filename:join([Folder, App, "ebin"]),
+    CompileResult = boss_load:maybe_compile(Filename, list_to_atom(App), OutDir, fun boss_load:compile_controller/2),
+    lager:notice("~p [controller] Compile Result ~p", [Type, CompileResult]);
+
+compile_cbapp(Type, App, Path = ["src", "model", _]) ->
+    Folder = atom_to_list(Type),
+    Filename = filename:join([Folder, App] ++ Path),
+    OutDir = filename:join([Folder, App, "ebin"]),
+    CompileResult = boss_load:maybe_compile(Filename, list_to_atom(App), OutDir, fun boss_load:compile_model/2),
+    lager:notice("~p [model] Compile Result ~p", [Type, CompileResult]).
+
+
 
